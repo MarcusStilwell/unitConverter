31fd4790e6e4105af0a0be6d2eb6004e6891208d
MarcusStilwell <marcusstilwell@icloud.com>
2021-06-28 10:15:43 -0700

    Initial Commit

---
+++ unitConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  unitConverter
+//
+//  Created by Marcus Stilwell on 6/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
