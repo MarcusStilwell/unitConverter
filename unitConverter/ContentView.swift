f8de6a30d71f530094991130b76404c5d7fd3540
MarcusStilwell <marcusstilwell@icloud.com>
2021-06-28 12:36:18 -0700

    Fixed conversion calculations. Changed pickers to segmented pickers for better visualization.

--- unitConverter/ContentView.swift
+++ unitConverter/ContentView.swift
@@ -16,30 +16,35 @@ struct ContentView: View {
     var valueFromString: Double{
         return Double(tempFromValueString) ?? 0.0
     }
+    
+    //base value will be Kelvin. If the original value is not in Kelvin, the value input by the user will be converted to Kelvin. Then if the unit being converted to is not Kelvin, the value is converted to the new value. Since Kelvin is the base value, there is not a calculation for tempFrom or tempTo if either of those values are Kelvin(0).
+    
     var convertedValue: Double{
-        var newTemp = 0.0
+        var newTemp = valueFromString
         if(tempFrom == 1){
-            newTemp = valueFromString+273
+            newTemp += 273
         }
         if(tempFrom == 2){
-            newTemp = (((valueFromString-32) * (5/9)) + 273.15)
+            newTemp = ((valueFromString - 273.15) * (9/5) + 32)
+            
         }
         if(tempTo == 1){
             newTemp += 273
         }
         if(tempTo == 2){
-            newTemp = ((newTemp - 273.15) * (9/5) + 32)
+            newTemp = (((valueFromString-32) * (5/9)) + 273.15)
         }
         return newTemp
     }
     var body: some View {
         Form{
             Section{
-                Picker("Choose a unit to convert to", selection: $tempTo){
+                Picker("Choose a unit to convert from", selection: $tempTo){
                     ForEach (0 ..< tempUnits.count) {
                         Text("\(self.tempUnits[$0])")
                     }
                 }
+                .pickerStyle(SegmentedPickerStyle())
             }
             Section{
                 TextField("Enter a the value for \(tempUnits[tempFrom])", text: $tempFromValueString).keyboardType(.decimalPad)
@@ -51,6 +56,7 @@ struct ContentView: View {
                         Text("\(self.tempUnits[$0])")
                     }
                 }
+                .pickerStyle(SegmentedPickerStyle())
             }
             Section{
                 Text("\(convertedValue, specifier: "%.2f")")
