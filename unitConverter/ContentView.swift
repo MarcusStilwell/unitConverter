2b55b19b86eed51aec3279410697b01cc57e2ea7
MarcusStilwell <marcusstilwell@icloud.com>
2021-06-28 10:37:20 -0700

    Added unit input fields and state variables.

--- unitConverter/ContentView.swift
+++ unitConverter/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tempFrom = 0
+    @State private var tempTo = 0
+    var tempUnits = ["Kelvin", "Celsius", "Farenheit"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form{
+            Section{
+                Picker("Choose a unit to convert to", selection: $tempTo){
+                    ForEach (0 ..< tempUnits.count) {
+                        Text("\(self.tempUnits[$0])")
+                    }
+                }
+            }
+            Section{
+                Picker("Choose a unit to convert to", selection: $tempFrom){
+                    ForEach (0 ..< tempUnits.count) {
+                        Text("\(self.tempUnits[$0])")
+                    }
+                }
+            }
+        }
     }
 }
 
